03431ed4c8f4c825e2a1d39872e810ee922bf88d
Takotubo <teira32@yahoo.co.jp>
2019-09-03 16:12:12 +0900

    propsうまくいかん状態

--- src/SelectMenu.tsx
+++ src/SelectMenu.tsx
@@ -1,3 +1,4 @@
+/* eslint-disable @typescript-eslint/explicit-function-return-type */
 import React, { useState } from 'react'
 import Button from '@material-ui/core/Button'
 import IconButton from '@material-ui/core/IconButton'
@@ -11,21 +12,23 @@ import Close from '@material-ui/icons/Close'
 import Check from '@material-ui/icons/Check'
 import NewFile from './Fplus.svg'
 import OpenFile from './Fopen.svg'
+import { RouteComponentProps, Route } from 'react-router-dom'
+import { withRouter } from 'react-router'
 
 interface State {
     words: string
     filename: string
-    saveaddress: string
 }
 
-const SelectMenu: React.FC = (): JSX.Element => {
-    const [mode, setMode] = useState(0)
-
+const SelectMenu: React.FC<RouteComponentProps> = (props): JSX.Element => {
     const [values, setValues] = useState<State>({
         words: '3',
         filename: '',
-        saveaddress: '',
     })
+
+    const handlePageChange = (address: string) => {
+        //props.history.push(address)
+    }
 
     const handleChange = (name: keyof State) => (
         event: React.ChangeEvent<HTMLInputElement>
@@ -33,70 +36,70 @@ const SelectMenu: React.FC = (): JSX.Element => {
         setValues({ ...values, [name]: event.target.value })
     }
 
+    const topPage = () => (
+        <div>
+            <Button
+                variant="text"
+                color="inherit"
+                onClick={() => handlePageChange('/NewFile')}
+            >
+                <Container className="ButtonContainer">
+                    <img className="ButtonImage" src={NewFile} />
+                    <p className="ButtonText">新規プロジェクト</p>
+                </Container>
+            </Button>
+            <Button variant="text" color="inherit">
+                <Container className="ButtonContainer">
+                    <img className="ButtonImage" src={OpenFile} />
+                    <p className="ButtonText">既存プロジェクト</p>
+                </Container>
+            </Button>
+            <br />
+            <br />
+            <div className="WordCount">
+                ワード数
+                <TextField
+                    className="WordCountInput"
+                    inputProps={{ style: { textAlign: 'right' } }}
+                    type="number"
+                    value={values.words}
+                    onChange={handleChange('words')}
+                    defaultValue="3"
+                />
+            </div>
+        </div>
+    )
+
+    const newFile = () => (
+        <Card>
+            <CardContent>
+                <TextField
+                    className="FileName"
+                    label="プロジェクト名"
+                    value={values.filename}
+                    onChange={handleChange('filename')}
+                />
+            </CardContent>
+            <CardActions className="CardButton">
+                <IconButton color="primary">
+                    <Check />
+                </IconButton>
+                <IconButton
+                    color="inherit"
+                    onClick={() => handlePageChange('/')}
+                >
+                    <Close />
+                </IconButton>
+            </CardActions>
+        </Card>
+    )
+
     return (
         <div>
-            {mode == 0 && (
-                <div>
-                    <Button
-                        variant="text"
-                        color="inherit"
-                        onClick={() => setMode(1)}
-                    >
-                        <Container className="ButtonContainer">
-                            <img className="ButtonImage" src={NewFile} />
-                            <p className="ButtonText">新規プロジェクト</p>
-                        </Container>
-                    </Button>
-                    <Button variant="text" color="inherit">
-                        <Container className="ButtonContainer">
-                            <img className="ButtonImage" src={OpenFile} />
-                            <p className="ButtonText">既存プロジェクト</p>
-                        </Container>
-                    </Button>
-                    <br />
-                    <br />
-                    <div className="WordCount">
-                        ワード数
-                        <TextField
-                            className="WordCountInput"
-                            inputProps={{ style: { textAlign: 'right' } }}
-                            type="number"
-                            value={values.words}
-                            onChange={handleChange('words')}
-                            defaultValue="3"
-                        />
-                    </div>
-                </div>
-            )}
-            {mode == 1 && (
-                <Card>
-                    <CardContent>
-                        <TextField
-                            className="FileName"
-                            label="プロジェクト名"
-                            value={values.filename}
-                            onChange={handleChange('filename')}
-                        />
-                        <br />
-                        <TextField
-                            className="FileName"
-                            label="保存先"
-                            value={values.saveaddress}
-                            onChange={handleChange('saveaddress')}
-                        />
-                    </CardContent>
-                    <CardActions className="CardButton">
-                        <IconButton color="primary">
-                            <Check />
-                        </IconButton>
-                        <IconButton color="inherit" onClick={() => setMode(0)}>
-                            <Close />
-                        </IconButton>
-                    </CardActions>
-                </Card>
-            )}
+            <Route path="/" exact compornent={topPage} />
+            <Route path="/NewFile" exact compornent={newFile} />
         </div>
     )
 }
 
-export default SelectMenu
+export default withRouter(SelectMenu)
